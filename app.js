9f36ce9299905838679f954d1439bebf67e66d51
Ankit Vyas <jinfaust8@gmail.com>
2022-10-10 20:54:49 +0530

    Add CRUD functionality to the model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const methodOverride = require('method-override')
 
 const Campground = require("./models/campground")
 
@@ -19,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/campGround', { useNewUrlParser: true
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('__method'))
 
 
 app.get('/', (req, res) => {
@@ -26,16 +29,41 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/campgrounds', async (req,res) => {
+app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('./campgrounds/index',{campgrounds})
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('./campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findById(id)
+    const campground = await Campground.findById(req.params.id)
     res.render("./campgrounds/show",{campground})
 })
 
+app.get('/campgrounds/:id/edit', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('./campgrounds/edit',{campground})
+
+})
+
+app.put('/campgrounds/:id', async (req, res) =>{
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req,res) =>{
+    const campground = await Campground.findByIdAndDelete(req.params.id)
+    res.redirect('/campgrounds')
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
