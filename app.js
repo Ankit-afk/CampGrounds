c6b26e4568bed21df8082dae903583ff73c740ab
Ankit Vyas <jinfaust8@gmail.com>
2022-10-18 06:25:52 +0530

    Add form validation and error handling middleware

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 
 const Campground = require("./models/campground")
-
+const wrapAsync = require('./utils/wrapAsync')
 const mongoose = require('mongoose');
 const { appendFileSync } = require('fs')
 
@@ -33,40 +33,46 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', wrapAsync(async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('./campgrounds/index',{campgrounds})
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('./campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post('/campgrounds', wrapAsync(async (req, res, next) => {
+        const campground = new Campground(req.body.campground)
+        // console.log(campground)
+        await campground.save()
+        res.redirect(`/campgrounds/${campground._id}`)
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
+    // console.log(req.params)
     const campground = await Campground.findById(req.params.id)
     res.render("./campgrounds/show",{campground})
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req,res) => {
+app.get('/campgrounds/:id/edit', wrapAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('./campgrounds/edit',{campground})
 
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) =>{
+app.put('/campgrounds/:id', wrapAsync(async (req, res) =>{
     const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req,res) =>{
+app.delete('/campgrounds/:id', wrapAsync(async (req,res) =>{
     const campground = await Campground.findByIdAndDelete(req.params.id)
     res.redirect('/campgrounds')
+}))
+
+app.use((err,req,res,next) => {
+    res.send("Something went wrong")
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
