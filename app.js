5fd2c793c6a4643c439d1cb2cde49eb4de6790c3
Ankit Vyas <jinfaust8@gmail.com>
2022-11-01 19:33:00 +0530

    Restructure for static asssets

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('__method'))
-
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/', (req, res) => {
     res.render("home")
